1ed700607c3ca9b001b2bf218ec85a3f309d2b6b
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-17 19:51:42 +0530

    Understand about the declaration of array and null how to create the arrays and how to access through Index numbers

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,50 @@ public class Main {
         int [] rnos = new int [5];
         // or directly
         int[] rnos2 = {23,12,45,32,15};
+        // int [] rnos2 = {23,12,45, 32, 15};
 
+        int [] ros;// declaration of array. ros is getting defined in the stack
+        ros = new int[5];// initialisation : actually here object is being created in the
+
+
+        System.out.println(ros[1]);
+
+        String [] arr = new String[4];
+        System.out.println(arr[0]);
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
