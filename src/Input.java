d66999997412bab1513bc5c3d7be020f889892ec
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-18 21:47:41 +0530

    Understand about how to take the input using the for loop and how to print the element of array using the for each loop and how to convert into toString() Method for printing Array

--- src/Input.java
+++ src/Input.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] arr = new int [5];
-        arr[0] = 23;
-        arr[1] = 45;
-        arr[2] = 233;
-        arr[3] = 543;
-        arr[4] = 3;
+//        int[] arr = new int [5];
+//        arr[0] = 23;
+//        arr[1] = 45;
+//        arr[2] = 233;
+//        arr[3] = 543;
+//        arr[4] = 3;
         // [23, 45, 233, 543, 3]
 //        System.out.println(arr[3]);
 
         //Input using for loops
+//
+//        System.out.println("Enter the Numbers : ");
+//        for(int i = 0; i < arr.length; i++){
+//
+//            arr[i] = input.nextInt();
+//        }
 
-        System.out.println("Enter the Numbers : ");
-        for(int i = 0; i < arr.length; i++){
-
-            arr[i] = input.nextInt();
-        }
-
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
 
 //        System.out.println("Display the Numbers : ");
 //        for(int i = 0; i < arr.length; i++){
@@ -35,6 +35,20 @@ public class Input {
 
 //        System.out.println(arr[5]); // Index out of bound error
 
+        //array of objects
+        String[] str = new String[4];
+        for(int i = 0; i < str.length; i++){
+            str[i] = input.next();
+        }
+
+        System.out.println(Arrays.toString(str));
+
+        //modify
+        str[1] = "Sufi";
+
+        System.out.println(Arrays.toString(str));
+
+ nbvgjfxghlkjhgfsdtfgyhuijo
 
     }
 }
