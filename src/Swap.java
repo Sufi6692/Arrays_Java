a3ffadf513751c213009df96de30d7dd2ef32017
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-22 15:53:44 +0530

    Understand Reversing the Arrays with help of Swap function

--- src/Swap.java
+++ src/Swap.java
@@ -4,7 +4,7 @@ public class Swap {
     public static void main(String[] args) {
         int[] arr = {1, 3, 23, 9, 18};
         swap(arr, 1,4);
-        System.out.println(Arrays.toString(arr));
+System.out.println(Arrays.toString(arr));
     }
     static void swap(int[]arr, int index1, int index2) {
             int temp = arr[index1];
