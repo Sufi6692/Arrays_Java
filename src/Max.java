4c4c9253cf8bb60bb4f3c3d449cba2659429ac79
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-22 15:30:34 +0530

    Understand how to find the Max number in Array and in a certain Range of Array

---
+++ src/Max.java
@@ -0,0 +1,25 @@
+public class Max {
+    public static void main(String[] args) {
+        int [] arr = {1, 3,  9, 18, 23};
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr,1,3));
+    }
+    static int max (int[]arr) {
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+    static int maxRange(int[]arr, int start, int end){
+        int maxVal = arr[start];
+        for(int i = start; i < end; i++){
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
